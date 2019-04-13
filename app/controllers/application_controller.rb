992ab9c5abb9f5e23dd5b6df211145c321a3ab9a
Sajan Kuttipuravan <skpsajan@gmail.com>
2019-04-13 21:44:00 +0530

    db updated and removed unnecessary methods from application_contoller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def hello
-    render html: "¡Hola World"
-  end
-
-  def goodbye
-    render html: "Goodbye"
-  end
 end
