3f615380979ea715811a1ebe4841115dd6823ef1
Sajan Kuttipuravan <skpsajan@gmail.com>
2019-04-14 10:07:50 +0530

    added index,create,new todo_params to controller

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,4 +1,25 @@
 class TodoController < ApplicationController
-  def home
+  def list
+    @todo = Todo.all
   end
+
+  def new
+   @todo = Todo.new
+  end
+
+  def create
+    @todo = Book.new(todo_params)
+
+    if @todo.save
+      redirect_to :action => 'list'
+    else
+      redirect_to :action => 'list'
+    end
+  end
+
+  def todo_params
+     params.require(:todo).permit(:content)
+  end
+
+
 end
