96814b0e8fe28b80e44a6900ead1c54c910efc48
Sajan Kuttipuravan <skpsajan@gmail.com>
2019-04-14 23:03:08 +0530

    updated value of field

--- db/migrate/20190413160944_create_todos.rb
+++ db/migrate/20190413160944_create_todos.rb
@@ -2,7 +2,7 @@ class CreateTodos < ActiveRecord::Migration[5.1]
   def change
     create_table :todos do |t|
       t.string :content
-      t.boolean :status, :default => true
+      t.boolean :completed_status, :default => false
 
       t.timestamps
     end
