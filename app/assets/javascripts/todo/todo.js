7f6e043750bb19b497f4b981ab60752a7573df85
Sajan Kuttipuravan <skpsajan@gmail.com>
2019-04-17 15:07:14 +0530

    added completed_status be saved on change using jquery

--- app/assets/javascripts/todo/todo.js
+++ app/assets/javascripts/todo/todo.js
@@ -1,3 +1,5 @@
+//= require rails-ujs
+
 $(document).on("turbolinks:load", function(){
   console.log("non editable run 1");
   make_non_editable_all();
@@ -12,3 +14,14 @@ function make_editable(x){
   console.log(a)
   $(a).prop("readonly",false);
 }
+
+function submit_this_entry(x){
+  a = '.custom_class_'+x;
+  elem = $(a)[0];
+  console.log(elem);
+  Rails.fire(elem, 'submit');
+}
+//var elem = document.getElementsByClassName("custom_class_107")[0]
+//console.log("testing the submit");
+//var elem =  $('#edit_form')[0];
+//Rails.fire(elem, 'submit');
