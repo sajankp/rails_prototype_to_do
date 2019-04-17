7915ade1888a8d776d480b32b7748e98443d94d6
Sajan Kuttipuravan <skpsajan@gmail.com>
2019-04-17 08:53:49 +0530

    jquery execute on turbolinks requests as well

--- app/assets/javascripts/todo/todo.js
+++ app/assets/javascripts/todo/todo.js
@@ -1,8 +1,11 @@
-window.onload=function(){
-  console.log("test");
-  $(".todos input[name='todo[content]']").prop("readonly",true);}
+$(document).on("turbolinks:load", function(){
+  console.log("non editable run 1");
+  make_non_editable_all();
+})
 
-
+function make_non_editable_all(){
+  $(".todos input[name='todo[content]']").prop("readonly",true);
+}
 
 function make_editable(x){
   var a = ".custom_class_"+ x +" input#todo_content";
