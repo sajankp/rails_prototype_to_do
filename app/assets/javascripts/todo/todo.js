e668b283c7303526ba054d49f2635e61cedd1208
Sajan Kuttipuravan <skpsajan@gmail.com>
2019-04-17 15:22:27 +0530

    converted into unique ids instead of unique classes

--- app/assets/javascripts/todo/todo.js
+++ app/assets/javascripts/todo/todo.js
@@ -10,18 +10,14 @@ function make_non_editable_all(){
 }
 
 function make_editable(x){
-  var a = ".custom_class_"+ x +" input#todo_content";
+  var a = "#edit_form_"+ x +" input#todo_content";
   console.log(a)
   $(a).prop("readonly",false);
 }
 
 function submit_this_entry(x){
-  a = '.custom_class_'+x;
+  a = '#edit_form_'+x;
   elem = $(a)[0];
   console.log(elem);
   Rails.fire(elem, 'submit');
 }
-//var elem = document.getElementsByClassName("custom_class_107")[0]
-//console.log("testing the submit");
-//var elem =  $('#edit_form')[0];
-//Rails.fire(elem, 'submit');
