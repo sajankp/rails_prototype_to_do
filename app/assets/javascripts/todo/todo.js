dbef6e280c1c4cc29e91157f0e2a61c21855b494
Sajan Kuttipuravan <skpsajan@gmail.com>
2019-04-18 23:03:57 +0530

    Add better debug messages for jquery and removed wrong css entry for menu li

--- app/assets/javascripts/todo/todo.js
+++ app/assets/javascripts/todo/todo.js
@@ -1,7 +1,7 @@
 //= require rails-ujs
 
 $(document).on("turbolinks:load", function(){
-  console.log("non editable run 1");
+  console.log("make all forms non editable and reset new form");
   document.getElementById("new_form").reset();
   make_non_editable_all();
 })
@@ -12,13 +12,13 @@ function make_non_editable_all(){
 
 function make_editable(x){
   var a = "#edit_form_"+ x +" input#todo_content";
-  console.log(x)
+  console.log("make form with the id "+x+" editable")
   $(a).prop("readonly",false);
 }
 
 function submit_this_entry(x){
   a = '#edit_form_'+x;
   elem = $(a)[0];
-  console.log(x);
+  console.log("check box clicked for form with id: "+x);
   Rails.fire(elem, 'submit');
 }
