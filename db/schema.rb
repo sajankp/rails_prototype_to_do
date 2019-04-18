1bb1dfe6ba3fd77d2879a300c016cb8b43ece84a
Sajan Kuttipuravan <skpsajan@gmail.com>
2019-04-18 08:32:47 +0530

    Add necessary gems to migrate test,dev db from sqlite to  postgreql
    inorder to replicate same conditions as in heroku during testing switching from sqlite to postgreql and hence changed database.yml and added gem pg version 1.0.0

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20190413160944) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "todos", force: :cascade do |t|
     t.string "content"
     t.boolean "completed_status", default: false
