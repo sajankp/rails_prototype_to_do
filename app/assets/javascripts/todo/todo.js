b4f68d85c2cbd45e5d4e3f9faea6011ed26f301e
Sajan Kuttipuravan <skpsajan@gmail.com>
2019-04-18 19:23:49 +0530

    form reset on refresh of page

--- app/assets/javascripts/todo/todo.js
+++ app/assets/javascripts/todo/todo.js
@@ -2,6 +2,7 @@
 
 $(document).on("turbolinks:load", function(){
   console.log("non editable run 1");
+  document.getElementById("new_form").reset();
   make_non_editable_all();
 })
 
@@ -11,13 +12,13 @@ function make_non_editable_all(){
 
 function make_editable(x){
   var a = "#edit_form_"+ x +" input#todo_content";
-  console.log(a)
+  console.log(x)
   $(a).prop("readonly",false);
 }
 
 function submit_this_entry(x){
   a = '#edit_form_'+x;
   elem = $(a)[0];
-  console.log(elem);
+  console.log(x);
   Rails.fire(elem, 'submit');
 }
